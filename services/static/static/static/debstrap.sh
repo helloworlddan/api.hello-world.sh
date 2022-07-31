24a41d658551098228512af80358855d052d7553
Daniel Stamer <dan@hello-world.sh>
2022-07-31 15:51:01 +0000

    include arch switch
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- services/static/static/static/debstrap.sh
+++ services/static/static/static/debstrap.sh
@@ -3,31 +3,43 @@
 set -eu
 
 sudo apt-get -y update
-sudo apt-get -y install git vim stow golang apt-transport-https ca-certificates gnupg jq ranger lsb-release htop fonts-jetbrains-mono gnome-keyring
-
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor  | sudo apt-key add - 
-echo "deb https://packages.microsoft.com/repos/vscode stable main" | sudo tee -a /etc/apt/sources.list.d/vscode.list
+sudo apt-get -y install libnss3 libnspr4 git vim stow golang apt-transport-https ca-certificates gnupg jq ranger lsb-release htop fonts-jetbrains-mono gnome-keyring
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 
 echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list 
-
-curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor  | sudo apt-key add - 
-echo "deb  https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/hashicorp.list
-
 sudo apt-get -y update
-sudo apt-get -y install code google-cloud-sdk terraform
+sudo apt-get -y install google-cloud-sdk
 
 mkdir -p "${HOME}/.local/bin"
-(
-    cd "${HOME}/.local/bin"
-    if [ $(uname -m) == "x86_64" ]
-    then
+arch="$(uname -m)"
+if [ "${arch}" = "x86_64" ]
+then
+    curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor  | sudo apt-key add - 
+    echo "deb  https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/hashicorp.list
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor  | sudo apt-key add - 
+    echo "deb https://packages.microsoft.com/repos/vscode stable main" | sudo tee -a /etc/apt/sources.list.d/vscode.list
+    sudo apt update
+    sudo apt-get install -y code terraform
+    (
+        cd "${HOME}/.local/bin"
         curl -Lo tresor https://github.com/helloworlddan/tresor/releases/download/v1.1.4/tresor_linux_amd64
-    else
+        chmod +x tresor
+    )
+elif [ "${arch}" = "aarch64" ]
+then
+    curl -L https://aka.ms/linux-arm64-deb > code_arm64.deb
+    sudo apt install ./code_arm64.deb
+    rm code_arm64.deb    
+    (
+        cd "${HOME}/.local/bin"
+        curl -L https://releases.hashicorp.com/terraform/1.2.6/terraform_1.2.6_linux_arm64.zip | zcat > terraform
+        chmod +x terraform
         curl -Lo tresor https://github.com/helloworlddan/tresor/releases/download/v1.1.4/tresor_linux_arm64
-    fi
-    chmod +x tresor
-)
+        chmod +x tresor
+    )
+else
+    echo "unknown architecture\n"
+fi
 
 mkdir -p "${HOME}/Code/"
 
