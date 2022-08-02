abbd9d42ef282dd3778d737097f95cce2ac74eb0
Daniel Stamer <dan@hello-world.sh>
2022-08-02 22:31:53 +0200

    compute stub
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- services/machine/server.go
+++ services/machine/server.go
@@ -10,6 +10,8 @@ import (
 	"os"
 
 	gin "github.com/gin-gonic/gin"
+
+	compute "google.golang.org/api/compute/v1"
 )
 
 //"https://remotedesktop.google.com/access/session/ca683f00-d51c-4f1a-af5e-5f9a25b3f4a8"
@@ -24,11 +26,12 @@ type Principal struct {
 
 // Config holds the complete context of the managed machine
 type Config struct {
-	Session string
-	Owner   string
-	Zone    string
-	Project string
-	Machine string
+	Session     string
+	Owner       string
+	Zone        string
+	Project     string
+	Machine     string
+	Environment string
 }
 
 var (
@@ -61,15 +64,29 @@ func configure() (Config, error) {
 	cfg.Zone = os.Getenv("TOP_ZONE")
 	cfg.Machine = os.Getenv("TOP_MACHINE")
 	cfg.Project = os.Getenv("GOOGLE_CLOUD_PROJECT")
+	cfg.Environment = os.Getenv("ENVIRONMENT")
 
-	if cfg.Session == "" || cfg.Owner == "" || cfg.Zone == "" || cfg.Machine == "" || cfg.Project == "" {
+	if cfg.Session == "" || cfg.Owner == "" || cfg.Zone == "" || cfg.Machine == "" || cfg.Project == "" || cfg.Environment == "" {
 		return Config{}, errors.New("config incomplete")
 	}
 	return cfg, nil
 }
 
 func GetHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, "unimplemented")
+	ctx := c.Request.Context()
+	gce, err := compute.NewService(ctx)
+	if err != nil {
+		log.Printf("failed to initialize GCE client: %v\n", err)
+	}
+
+	instances := compute.NewInstancesService(gce)
+	getCall := instances.Get(config.Project, config.Zone, config.Machine)
+	instance, err := getCall.Do()
+	if err != nil {
+		log.Printf("failed to initialize GCE client: %v\n", err)
+	}
+
+	c.JSON(http.StatusOK, instance.Status)
 }
 
 func PatchHandler(c *gin.Context) {
@@ -81,35 +98,45 @@ func DeleteHandler(c *gin.Context) {
 }
 
 func UserContextFromAPI(c *gin.Context) {
-	encoded := c.Request.Header.Get("X-Endpoint-API-UserInfo")
-	if encoded == "" {
-		log.Printf("error: %v\n", fmt.Errorf("missing gateway user info header"))
-		c.JSON(http.StatusUnauthorized, "missing gateway user info header")
-		c.Abort()
-		return
-	}
-	bytes, err := base64.RawURLEncoding.DecodeString(encoded)
-	if err != nil {
-		log.Printf("error: %v\n", err)
-		c.JSON(http.StatusUnauthorized, "failed to decode user info header")
-		c.Abort()
-		return
-	}
-
 	var caller Principal
-	err = json.Unmarshal(bytes, &caller)
-	if err != nil {
-		log.Printf("error: %v\n", err)
-		c.JSON(http.StatusUnauthorized, "failed to deserialize user info header")
-		c.Abort()
-		return
-	}
 
-	if caller.Email != config.Owner {
-		log.Println("error: unauthorized caller")
-		c.JSON(http.StatusUnauthorized, "failed to deserialize user info header")
-		c.Abort()
-		return
+	if config.Environment != "dev" {
+		encoded := c.Request.Header.Get("X-Endpoint-API-UserInfo")
+		if encoded == "" {
+			log.Printf("error: %v\n", fmt.Errorf("missing gateway user info header"))
+			c.JSON(http.StatusUnauthorized, "missing gateway user info header")
+			c.Abort()
+			return
+		}
+		bytes, err := base64.RawURLEncoding.DecodeString(encoded)
+		if err != nil {
+			log.Printf("error: %v\n", err)
+			c.JSON(http.StatusUnauthorized, "failed to decode user info header")
+			c.Abort()
+			return
+		}
+
+		err = json.Unmarshal(bytes, &caller)
+		if err != nil {
+			log.Printf("error: %v\n", err)
+			c.JSON(http.StatusUnauthorized, "failed to deserialize user info header")
+			c.Abort()
+			return
+		}
+
+		if caller.Email != config.Owner {
+			log.Println("error: unauthorized caller")
+			c.JSON(http.StatusUnauthorized, "failed to deserialize user info header")
+			c.Abort()
+			return
+		}
+	} else {
+		caller = Principal{
+			ID:         "1",
+			Name:       "dev user",
+			Email:      "stay@puft.gb",
+			PictureURL: "https://www.eric-andreu.com/wp-content/uploads/2021/12/account.eric-andreu-956x1024.png",
+		}
 	}
 
 	// Context OK
