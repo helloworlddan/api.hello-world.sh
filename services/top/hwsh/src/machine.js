b8d994b397477b7f226d16f751f941eb54c950bc
Daniel Stamer <dan@hello-world.sh>
2022-08-03 13:52:41 +0200

    add control buttons
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

---
+++ services/top/hwsh/src/machine.js
@@ -0,0 +1,15 @@
+
+export const machineStatus = () => {
+    // TODO implement
+    alert("unimplemented");
+};
+
+export const machineStart = () => {
+    // TODO implement
+    alert("unimplemented");
+};
+
+export const machineStop = () => {
+    // TODO implement
+    alert("unimplemented");
+};
