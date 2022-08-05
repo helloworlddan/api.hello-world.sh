508b59dcd21d7d70cfc8fc99f6dc58989cfdb40f
Daniel Stamer <dan@hello-world.sh>
2022-08-05 13:14:37 +0200

    log api errors
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- services/top/hwsh/src/machine.js
+++ services/top/hwsh/src/machine.js
@@ -1,42 +1,36 @@
 const endpoint = 'https://api.hello-world.sh/machine/';
 
-export const machineStatus = async (token) => {
-    await fetch(endpoint, {
-        method: "GET",
+const executeRequest = async (requestMethod, token) => {
+    const response = await fetch(endpoint, {
+        method: requestMethod,
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'Authorization': `Bearer: ${token}`
         }
-    }).then((response) => {
-        const result = response.json();
+    })
+
+    if (!response.ok) {
+        console.log(response);
+    }
+    
+    return await response.json();
+}
+
+export const machineStatus = async (token) => {
+    executeRequest("GET", token).then((result) => {
         alert(`machine status: ${result['status']}`)
     });
 };
 
 export const machineStart = async (token) => {
-    await fetch(endpoint, {
-        method: "PATCH",
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': `Bearer: ${token}`
-        }
-    }).then((response) => {
-        const result = response.json();
+    executeRequest("PATCH", token).then((result) => {
         alert(`machine: ${result['message']}, redirect: ${result['redirect_link']}`)
     });
 };
 
 export const machineStop = async (token) => {
-    await fetch(endpoint, {
-        method: "DELETE",
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': `Bearer: ${token}`
-        }
-    }).then((response) => {
+    executeRequest("DELETE", token).then((response) => {
         const result = response.json();
         alert(`machine: ${result['message']}`)
     });
