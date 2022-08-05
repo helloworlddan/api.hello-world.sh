7b967f47e6be098f2865adfeca54a2fc9c5236cf
Daniel Stamer <dan@hello-world.sh>
2022-08-05 11:57:02 +0200

    impl async
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- services/top/hwsh/src/machine.js
+++ services/top/hwsh/src/machine.js
@@ -1,58 +1,43 @@
 const endpoint = 'https://api.hello-world.sh/machine/';
 
-export const machineStatus = (token) => {
-    const response = await fetch(endpoint, {
+export const machineStatus = async (token) => {
+    await fetch(endpoint, {
         method: "GET",
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'Authorization': `Bearer: ${token}`
         }
+    }).then((response) => {
+        const result = response.json();
+        alert(`machine status: ${result['status']}`)
     });
-
-    if (!response.ok) {
-        alert(`error: ${response.status}`);
-    }
-
-    const result = await response.json();
-
-    alert(`machine status: ${result['status']}`)
 };
 
-export const machineStart = (token) => {
-    const response = await fetch(endpoint, {
+export const machineStart = async (token) => {
+    await fetch(endpoint, {
         method: "PATCH",
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'Authorization': `Bearer: ${token}`
         }
+    }).then((response) => {
+        const result = response.json();
+        alert(`machine: ${result['message']}, redirect: ${result['redirect_link']}`)
     });
-
-    if (!response.ok) {
-        alert(`error: ${response.status}`);
-    }
-
-    const result = await response.json();
-
-    alert(`machine: ${result['message']}, redirect: ${result['redirect_link']}`)
 };
 
 export const machineStop = (token) => {
-    const response = await fetch(endpoint, {
+    await fetch(endpoint, {
         method: "DELETE",
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'Authorization': `Bearer: ${token}`
         }
+    }).then((response) => {
+        const result = response.json();
+        alert(`machine: ${result['message']}`)
     });
-
-    if (!response.ok) {
-        alert(`error: ${response.status}`);
-    }
-
-    const result = await response.json();
-
-    alert(`machine: ${result['message']}`)
 };
