8d4daba68aad73fd01b4ea0888db15980e267a14
Daniel Stamer <dan@hello-world.sh>
2022-08-05 11:25:39 +0200

    call impl
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- services/top/hwsh/src/components/Dashboard.jsx
+++ services/top/hwsh/src/components/Dashboard.jsx
@@ -4,15 +4,13 @@ import {auth} from "../firebase";
 import {machineStatus, machineStart, machineStop} from "../machine";
 const Dashboard = () => {
   const user = useContext(UserContext);
-  const {photoURL, displayName, email, uid, _lat} = user;
-  console.log(user);
-  
+  const {displayName} = user;
 
   return (
     <div className = "mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
       <div className="flex border flex-col items-center md:flex-row md:items-start border-blue-400 px-3 py-4">
         <div>
-        <h3 className = "text-2xl font-semibold">Welcome, {displayName} ({email})!</h3>
+        <h3 className = "text-2xl font-semibold">Welcome, {displayName}!</h3>
         </div>
       </div>
       <button className = "w-full py-3 bg-yellow-600 mt-4 text-white" onClick = {() => {auth.signOut()}}>Sign out</button>
@@ -22,30 +20,6 @@ const Dashboard = () => {
       <button className = "w-full py-3 bg-green-600 mt-4 text-white" onClick = {() => {machineStart()}}>Start</button>
 
       <button className = "w-full py-3 bg-red-600 mt-4 text-white" onClick = {() => {machineStop()}}>Stop</button>
-
-      <div className = "md:pl-4">Debug Info</div>
-      <div>
-        <table style={{
-            tableLayout: "fixed"
-            }}>
-        <tbody style={{
-            border: "1px solid black"
-            }}>
-          <tr style={{
-            border: "1px solid black"
-            }}>
-            <td><code>{uid}</code></td>
-          </tr >
-          <tr style={{
-            border: "1px solid black"
-            }}>
-            <td style={{
-            wordBreak: "break-all"
-            }}><code>{_lat}</code></td>
-          </tr>
-        </tbody>
-        </table>
-      </div>
     </div>
   ) 
 };
--- services/top/hwsh/src/machine.js
+++ services/top/hwsh/src/machine.js
@@ -1,15 +1,70 @@
+import { useContext } from "react";
+import { UserContext } from "../providers/UserProvider";
+
+const endpoint = 'https://api.hello-world.sh/machine/';
 
 export const machineStatus = () => {
-    // TODO implement
-    alert("unimplemented");
+    const user = useContext(UserContext);
+    const {_lat} = user;
+
+    const response = fetch(endpoint, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer: ${_lat}`
+        }
+    });
+
+    if (!response.ok) {
+        alert(`error: ${response.status}`);
+    }
+
+    const result = response.json();
+
+    alert(`machine status: ${result['status']}`)
 };
 
 export const machineStart = () => {
-    // TODO implement
-    alert("unimplemented");
+    const user = useContext(UserContext);
+    const {_lat} = user;
+
+    const response = fetch(endpoint, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer: ${_lat}`
+        }
+    });
+
+    if (!response.ok) {
+        alert(`error: ${response.status}`);
+    }
+
+    const result = response.json();
+
+    alert(`machine: ${result['message']}, redirect: ${message['redirect_link']}`)
 };
 
 export const machineStop = () => {
-    // TODO implement
-    alert("unimplemented");
+    const user = useContext(UserContext);
+    const {_lat} = user;
+
+    const response = fetch(endpoint, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer: ${_lat}`
+        }
+    });
+
+    if (!response.ok) {
+        alert(`error: ${response.status}`);
+    }
+
+    const result = response.json();
+
+    alert(`machine: ${result['message']}`)
 };
