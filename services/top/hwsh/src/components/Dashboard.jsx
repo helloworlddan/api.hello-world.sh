204ec345712bc2879ffb738971cdc2d7b0ac1b7b
Daniel Stamer <dan@hello-world.sh>
2022-08-07 10:49:36 +0200

    check machine initialization
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- services/top/hwsh/src/components/Dashboard.jsx
+++ services/top/hwsh/src/components/Dashboard.jsx
@@ -66,9 +66,17 @@ const Dashboard = () => {
           <h3 className="text-2xl font-semibold">Welcome, {displayName}!</h3>
         </div>
         <div>
-          <p>Message: {machine.message || "-"}</p>
-          <p>Machine status: {machine.status || "-"}</p>
-          <p>Redirect: {machine.redirect_link || "-"}</p>
+          {
+            machine => {
+            (
+              <div>
+                <p>Message: {machine["message"] || "-"}</p>
+                <p>Machine status: {machine["status"] || "-"}</p>
+                <p>Redirect: {machine["redirect_link"] || "-"}</p>
+              </div>
+            )
+          }
+        }
         </div>
       </div>
       <button className="w-full py-3 bg-yellow-600 mt-4 text-white" onClick={() => { auth.signOut() }}>Sign out</button>
