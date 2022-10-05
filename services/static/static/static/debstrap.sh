a13dfe4bd8f015f200c032f44eee4d82485d8a6e
Daniel Stamer <dan@hello-world.sh>
2022-10-05 15:53:40 +0200

    Update debstrap.sh
    add go tools

--- services/static/static/static/debstrap.sh
+++ services/static/static/static/debstrap.sh
@@ -43,6 +43,7 @@ else
     echo "unknown architecture\n"
 fi
 
+mkdir -p "${HOME}/.go/"
 mkdir -p "${HOME}/Code/"
 
 mkdir -p "${HOME}/.dotfiles"
@@ -52,6 +53,35 @@ mkdir -p "${HOME}/.dotfiles"
     stow dotfiles 
 )
 echo '\nsource "${HOME}/.bash_profile"\n' >> "${HOME}/.bashrc"
+
+sudo apt-get autoremove -y
+
+go install -v golang.org/x/tools/gopls@latest
+go install -v github.com/go-delve/delve/cmd/dlv@latest
+go install -v github.com/mdempsky/gocode@latest
+go install -v github.com/stamblerre/gocode@latest
+go install -v github.com/ramya-rao-a/go-outline@latest
+go install -v github.com/acroca/go-symbols@latest
+go install -v golang.org/x/tools/cmd/guru@latest
+go install -v golang.org/x/tools/cmd/gorename@latest
+go install -v github.com/fatih/gomodifytags@latest
+go install -v github.com/haya14busa/goplay/cmd/goplay@latest
+go install -v github.com/josharian/impl@latest
+go install -v github.com/tylerb/gotype-live@latest
+go install -v github.com/rogpeppe/godef@latest
+go install -v github.com/zmb3/gogetdoc@latest
+go install -v mvdan.cc/gofumpt/gofumports@latest
+go install -v mvdan.cc/gofumpt@latest
+go install -v golang.org/x/tools/cmd/goimports@latest
+go install -v github.com/sqs/goreturns@latest
+go install -v winterdrache.de/goformat/goformat@latest
+go install -v github.com/cweill/gotests/gotests@latest
+go install -v golang.org/x/lint/golint@latest
+go install -v honnef.co/go/tools/cmd/staticcheck@latest
+go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install -v github.com/mgechev/revive@latest
+go install -v github.com/mgechev/revive@latest
+go install -v github.com/godoctor/godoctor@latest
 
 gcloud auth login
 gcloud auth application-default login
